e1f82411bfed4bcd728e1eba0a74c9d161d619f6
DenSerKo <89607400164denis@gmail.com>
2024-03-22 21:58:12 +0300

    Аленушка жопушка

--- ai/views.py
+++ ai/views.py
@@ -1,5 +1,19 @@
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'ai/index.html'
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}.')
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
